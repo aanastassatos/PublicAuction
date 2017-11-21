25d908dde22aeb6a7f033bf38f28f27212a3a4fe
Catherine Wright <wrightc@unm.edu>
2017-11-21 15:19:33 -0700

    message

--- src/Agent/Agent.java
+++ src/Agent/Agent.java
@@ -1,5 +1,22 @@
 package Agent;
 
-public class Agent
+import java.io.*;
+import java.net.Socket;
+
+public class Agent extends Thread
 {
+  private Socket socket;
+  private ObjectOutputStream oos;
+  private ObjectInputStream ois;
+
+  public static void main(String[] args) throws Exception
+  {
+    String ip = "localhost";
+    int port = 9999;
+    Socket socket = new Socket(ip, port);
+
+    OutputStreamWriter osw = new OutputStreamWriter(socket.getOutputStream());
+    PrintWriter printWriter = new PrintWriter(osw);
+
+  }
 }
