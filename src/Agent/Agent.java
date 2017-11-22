7cac4e3d9c0fc0e31e273d6f4aa679a57c4c0e45
Catherine Wright <wrightc@unm.edu>
2017-11-21 22:35:49 -0700

    Merge branch 'master' of /Users/catherinewright/Documents/CS351/PublicAuction with conflicts.

--- src/Agent/Agent.java
+++ src/Agent/Agent.java
@@ -1,22 +1,15 @@
 package Agent;
 
-import java.io.*;
-import java.net.Socket;
-
 public class Agent extends Thread
 {
-  private Socket socket;
-  private ObjectOutputStream oos;
-  private ObjectInputStream ois;
+
+  public Agent()
+  {
+    AgentBankAccount bankAccount = new AgentBankAccount();
+  }
 
   public static void main(String[] args) throws Exception
   {
-    String ip = "localhost";
-    int port = 9999;
-    Socket socket = new Socket(ip, port);
-
-    OutputStreamWriter osw = new OutputStreamWriter(socket.getOutputStream());
-    PrintWriter printWriter = new PrintWriter(osw);
-
+    Agent agent = new Agent();
   }
 }
