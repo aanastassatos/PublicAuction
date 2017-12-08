769f9095a0e475c4fc5b397625ffea0d606731b5
Catherine Wright <wrightc@unm.edu>
2017-12-08 10:53:57 -0700

    commented and cleaned agent

--- src/Agent/AgentAuctionHouse.java
+++ src/Agent/AgentAuctionHouse.java
@@ -1,14 +1,24 @@
 package Agent;
 
-import AuctionHouse.Item;
+/**
+ * Agent package:
+ * AgentAuctionHouse class creates a new auction house socket to connect
+ * to the agent, and simulates the bid
+ */
+
 import Messages.*;
-import javafx.application.Platform;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.HashMap;
+
+/**
+ * Agent package:
+ * AgentAuctionHouse class: creates a new thread connected to the
+ * auction house. Handles bidding and item information between the
+ * auction house and the agent
+ */
 
 public class AgentAuctionHouse extends Thread
 {
@@ -17,6 +27,14 @@ public class AgentAuctionHouse extends Thread
   private ObjectOutputStream oos;
   private ObjectInputStream ois;
 
+  /**
+   * Constructor
+   * @param address
+   * @param port
+   * @param biddingKey
+   * @param agent
+   * @throws IOException
+   */
   AgentAuctionHouse(String address, int port, int biddingKey, Agent agent) throws IOException
   {
     this.agent = agent;
@@ -26,7 +44,20 @@ public class AgentAuctionHouse extends Thread
     ois = new ObjectInputStream(houseSocket.getInputStream());
     oos.writeObject(new AgentInfoMessage(biddingKey));
   }
-  
+
+  /**
+   * run method for when AgentAuctionHouse thread starts
+   * continuously reads new messages and handles them according to
+   * type
+   * Messages Handled:
+   * ItemListMessage
+   * BidResultMessage
+   * ItemSoldMessage
+   * HigherBidPlacedMessage
+   * SuccessfulBidMessage
+   * NoItemLeftMessage
+   * CloseConnectionMessage
+   */
   @Override
   public void run()
   {
@@ -57,7 +88,11 @@ public class AgentAuctionHouse extends Thread
       else throw new RuntimeException("Received unknown message");
     }
   }
-  
+
+  /**
+   * writes the input object to the socket stream
+   * @param o: Message to be sent
+   */
   void sendMessage(Object o)
   {
     try
@@ -68,7 +103,10 @@ public class AgentAuctionHouse extends Thread
       e.printStackTrace();
     }
   }
-  
+
+  /**
+   * closes the connection to the socket
+   */
   void closeConnection()
   {
     try
@@ -80,7 +118,11 @@ public class AgentAuctionHouse extends Thread
       e.printStackTrace();
     }
   }
-  
+
+  /**
+   * handles an ItemListMessage writes a new item list
+   * @param msg
+   */
   private void handleMessage(ItemListMessage msg)
   {
     try
@@ -91,114 +133,41 @@ public class AgentAuctionHouse extends Thread
       e.printStackTrace();
     }
   }
-  
+
+  /**
+   * handles a bid result message, sending it to the auction house
+   * @param msg
+   */
   private void handleMessage(BidResultMessage msg)
   {
     agent.handleBidResult(msg.getResult());
   }
-  
+
+  /**
+   * handles an item sold message, to alert the agent the item is sold
+   * @param msg
+   */
   private void handleMessage(ItemSoldMessage msg)
   {
     System.out.println(msg.getItemName()+" was sold for "+msg.getAmount());
   }
-  
+
+  /**
+   * handles a Higher bid placed message, to alert the bidder of a higher bid
+   * by another agent
+   * @param msg
+   */
   private void handleMessage(HigherBidPlacedMessage msg)
   {
     System.out.println(msg.getItemName()+" has recieved a bid for $"+msg.getAmount());
   }
-  
+
+  /**
+   * handles a successful bid message, to alert the buyer they won the auction
+   * @param msg
+   */
   private void handleMessage(SuccessfulBidMessage msg)
   {
     System.out.println("You won the "+msg.getItemName()+" for $"+msg.getAmount()+"!");
   }
-  
-//  void setItemToBid(Item item)
-//  {
-//    this.itemToBid = item.getID();
-//  }
-//
-//  void setAmountToBid(int bidAmount)
-//  {
-//    this.amountToBid = bidAmount;
-//  }
-//
-//  public void connectToHouse(String address, int port)
-//  {
-//
-//  }
-//
-//  String getSuccess() {
-//    return success;
-//  }
-//
-//  void startAuction()
-//  {
-//    try
-//    {
-//      while (true)
-//      {
-//        if (itemToBid != null)
-//        {
-//          oos.writeObject(new BidPlacedMessage(biddingKey, itemToBid, amountToBid));
-//          itemToBid = null;
-//        }
-//        Object readMessage = ois.readObject();
-//        if (readMessage instanceof ItemListMessage)
-//        {
-//          ItemListMessage listMessage = (ItemListMessage) readMessage;
-//          items = listMessage.getItemList();
-//          //agent.setItems(items);
-//        } else if (readMessage instanceof SuccessfulBidMessage) handleMessage((SuccessfulBidMessage) readMessage);
-//        else if (readMessage instanceof BidResultMessage) handleMessage((BidResultMessage) readMessage);
-//        else if (readMessage instanceof NoItemLeftMessage)
-//        {
-//          oos.writeObject(new CloseConnectionMessage());
-//          break;
-//        }
-//      }
-//    }
-//    catch(Exception e)
-//    {
-//      e.printStackTrace();
-//    }
-//  }
-//
-//  HashMap<Integer, Item> getItems()
-//  {
-//    while(items == null)
-//    {
-//
-//    }
-//    return items;
-//  }
-//
-//  private void handleMessage(SuccessfulBidMessage message)
-//  {
-//    success = "You Won!";
-//  }
-//
-//  private void handleMessage(BidResultMessage message)
-//  {
-//    String result;
-//    BidResultMessage.BidResult bidResult = message.getResult();
-//    switch(bidResult)
-//    {
-//      case BID_IS_TOO_LOW:
-//        result = "Bid is too low!";
-//        break;
-//      case SUCCESS:
-//        result = "Bid Placed!";
-//        break;
-//      case INSUFFICIENT_FUNDS:
-//        result = "Insufficient Funds in Your Account :(";
-//        break;
-//      case NOT_IN_STOCK:
-//        result = "Item Not In Stock";
-//        break;
-//      default:
-//        result = "Here Be Monsters";
-//        break;
-//    }
-//    Platform.runLater(() -> new AgentSuccessGUI(result));
-//  }
 }
