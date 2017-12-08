50c0556a006839474eae0a9f512f1222d3f55974
pnt204 <pntran95@gmail.com>
2017-12-08 11:22:55 -0700

    AuctionDesign

--- src/AuctionHouse/AuctionHouse.java
+++ src/AuctionHouse/AuctionHouse.java
@@ -17,7 +17,8 @@ public class AuctionHouse extends Thread
   private final int numOfItems = 3;
 
   final static Random rand = new Random();
-  private final static int PORT = rand.nextInt((60000 - 50000) + 1) + 50000;
+  //private final static int PORT = rand.nextInt((60000 - 50000) + 1) + 50000;
+  private final static int PORT = 55557;
   private static String address;
 
   private int secretKey;
@@ -40,7 +41,7 @@ public class AuctionHouse extends Thread
       reader = new BufferedReader(new InputStreamReader(System.in));
       if(args.length > 0 && args[0].equals("test")) centralAddress = "";
       else centralAddress = reader.readLine();
-      address = "localhost";
+      address = centralAddress;
       Random r = new Random();
       char c = (char) (r.nextInt(26) + 'A');
       String name = Character.toString(c) + Integer.toString(r.nextInt(1000) + 1);
